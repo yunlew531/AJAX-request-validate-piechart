0fc4a718b51d0f4982656393f0e999b97b6d70a2
yunlew531 <yunlew531@gmail.com>
2021-03-31 05:56:04 +0800

    c3 piechart

--- js/main.js
+++ js/main.js
@@ -1,168 +1,162 @@
-// 渲染旅遊卡片
-function renderTourCard(arr) {
-  const searchResultText = document.querySelector('#searchResult-text');
-  const ticketCardArea = document.querySelector('.ticketCard-area');
+// 渲染卡片
+function renderTicket(arr) {
   let str = '';
   arr.forEach(item => {
     const content = `
-      <li class="ticketCard">
-        <div class="ticketCard-img">
-          <a href="#">
-            <img src="${item.imgUrl}" alt="">
-          </a>
-          <div class="ticketCard-region">${item.area}</div>
-          <div class="ticketCard-rank">${item.rate}</div>
+      <li class="card">
+        <div class="card-region">${item.area}</div>
+        <div class="card-header">
+          <img src="${item.imgUrl}"
+                alt="">
         </div>
-        <div class="ticketCard-content">
-          <div>
-            <h3>
-              <a href="#" class="ticketCard-name">${item.name}</a>
-            </h3>
-            <p class="ticketCard-description">
-              ${item.description}
-            </p>
-          </div>
-          <div class="ticketCard-info">
-            <p class="ticketCard-num">
-              <span><i class="fas fa-exclamation-circle"></i></span>
-              剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
-            </p>
-            <p class="ticketCard-price">
-              TWD <span id="ticketCard-price">$${item.price}</span>
-            </p>
+        <div class="card-main">
+          <div class="card-rate">8.7</div>
+          <h3>
+            <a href="#">${item.name}</a>
+          </h3>
+          <p class="card-description">
+          ${item.description}
+          </p>
+          <div class="card-footer">
+            <p class="cardNum-text"><i class="fas fa-exclamation-circle"></i>剩下最後 <span>${item.group}</span> 組</p>
+            <p>TWD<span class="card-price">$${item.price}</span></p>
           </div>
         </div>
       </li>
     `;
     str += content;
-  });
-  ticketCardArea.innerHTML = str;
-  if (regionSearch.value !== '地區搜尋')
-  searchResultText.textContent = `搜尋到 ${arr.length} 筆資料`;
+  })
+  if (areaSelect.value === '地區搜尋') {
+    cardList.innerHTML = '';
+  } else {
+    filterText.textContent = `本次搜尋共 ${arr.length} 筆資料`;
+  }
+  cardList.innerHTML = str;
+  addTicketForm.reset();
 }
 
-// 新增套票
-function addTourTicket() {
-  const ticketName = document.querySelector('#ticketName');
-  const ticketImgUrl = document.querySelector('#ticketImgUrl');
-  const ticketRegion = document.querySelector('#ticketRegion');
-  const ticketPrice = document.querySelector('#ticketPrice');
-  const ticketNum = document.querySelector('#ticketNum');
-  const ticketRate = document.querySelector('#ticketRate');
-  const ticketDescription = document.querySelector('#ticketDescription');
-  const addTicketForm = document.querySelector('.addTicket-form');
+function renderPieChart() {
+  let obj = {};
+  data.forEach(item => {
+    obj[item.area] = (obj[item.area] ? obj[item.area] + 1 : 1);
+  })
+  const arr = Object.keys(obj);
+  let areaArr = [];
+  arr.forEach(item => {
+    const arr = [item, obj[item]];
+    areaArr.push(arr);
+  });
+  areaArr = [areaArr[1], areaArr[2], areaArr[0]];
+  const chart = c3.generate({
+    bindto: '#areaPieChart',
+    data: {
+      columns: areaArr,
+      type: 'donut',
+    },
+    donut: {
+      title: "分布圖",
+      width: 7
+    },
+    color: {
+      pattern: ['#26C0C7', '#5151D3', '#E68618']
+    },
+    size: {
+      width: 160,
+      height: 160
+    },
+  });
+}
+
+// 新增卡片
+function addTicket() {
   const obj = {
-    id: Date.now(),
-    name: ticketName.value,
-    imgUrl: ticketImgUrl.value,
-    area: ticketRegion.value,
+    area: ticketArea.value,
     description: ticketDescription.value,
+    id: Date.now(),
+    imgUrl: ticketImgUrl.value,
+    name: ticketName.value,
     group: parseInt(ticketNum.value),
     price: parseInt(ticketPrice.value),
-    rate: parseInt(ticketRate.value)
+    rate: parseInt(ticketRate.value),
   };
-  const hasError = validate(false, obj);  // 表單驗證
+  const hasError = validate(false, obj);
   if (hasError) return;
   data.push(obj);
-  addTicketForm.reset();
-  renderTourCard(data);
-  renderChart();
+  renderTicket(data);
+  renderPieChart();
 }
 
-// 表單驗證
-function validate(event, obj) {
-  if (!event) {
-    const arr = Object.values(obj);
-    const isEmpty = arr.some(item => !item);
-    const isRateOutRange = (obj.rate < 1 || obj.rate > 10);
-    const isGroupZero = obj.group < 1;
-    const isPriceZero = obj.price < 1;
-    inputDom.forEach(item => {
-      const alertDom = item.parentNode.parentNode.querySelector('p');
-      if (!item.value) {
-        alertDom.innerHTML = validateMessage('empty');
-      } else if (item === ticketRate && isRateOutRange) {
-        alertDom.innerHTML = validateMessage('rate');
-      } else if (item === ticketPrice && isPriceZero) {
-        alertDom.innerHTML = validateMessage('priceZero');
-      } else if (item === ticketNum && isGroupZero) {
-        alertDom.innerHTML = validateMessage('NumZero');
+// 驗證表單
+function validate(e, obj) {
+  if (!e) {
+    const isEmpty = Object.values(obj).some(item => (item === 0 ? false : !item));  // 只寫 (item => !item) 金額0時無法新增
+    const isNumZero = obj.group < 1;
+    const isPriceZero = obj.price < 0;
+    const isRateOutRange = obj.rate < 1 || obj.rate > 10;
+    inputDoms.forEach(inputDom => {
+      const alertDom = inputDom.parentNode.querySelector('.alert-message');
+      if (inputDom === ticketNum && isNumZero) {
+        alertDom.innerHTML = alertMessage('num');
+      } else if (inputDom === ticketPrice && isPriceZero) {
+        alertDom.innerHTML = alertMessage('price');
+      } else if (inputDom === ticketRate && isRateOutRange) {
+        alertDom.innerHTML = alertMessage('rate');
+      } else if (!inputDom.value) {
+        alertDom.innerHTML = alertMessage('noWords');
       } else {
         alertDom.innerHTML = '';
       }
     });
-    return (isEmpty || isRateOutRange || isGroupZero || isPriceZero);
+    return isEmpty || isNumZero || isPriceZero || isRateOutRange;
   }
-  const alertDom = event.target.parentNode.parentNode.querySelector('p');
-  if (!event.target.value) {
-    alertDom.innerHTML = validateMessage('empty');
+  const alertDom = e.target.parentNode.querySelector('.alert-message');
+  if (!e.target.value) {
+    alertDom.innerHTML = alertMessage('noWords');
   } else {
     alertDom.innerHTML = '';
   }
 }
 
-// 表單驗證提醒訊息
-function validateMessage(text) {
-  const obj = {
-    empty: '這個選項必填!',
-    priceZero: '金額必須大於零!',
-    NumZero: '組數必須大於零!',
-    rate: '這個選項請填寫1~10!'
+// 驗證表單提示文字
+function alertMessage(text) {
+  const alertText = {
+    num: '組數必須大於零!',
+    price: '金額必須大於、等於零!',
+    rate: '這個選項請輸入1~10!',
+    noWords: '這個選項必填!',
   }
-  return `<i class="fas fa-exclamation-circle"></i>
-         <span>${obj[text]}</span>`;
+  return `<span class="alert-text">${alertText[text]}</span>`;
 }
 
 // 篩選地區
 function filterArea(e) {
-  const filterAreaArr = data.filter(item => item.area.match(e.target.value))
-  renderTourCard(filterAreaArr);
+  const filterArr = data.filter(item => item.area.match(e.target.value));
+  renderTicket(filterArr);
 }
 
-// 渲染圓餅圖
-function renderChart() {
-  let obj = {}
-  data.forEach(item => {
-    if (!obj[item.area]) 
-      obj[item.area] = 0;
-    obj[item.area]++;
-  })
-  let areaPieChartArr = [];
-  const arr = Object.keys(obj);
-  arr.forEach(item => {
-    const arr = [];
-    arr.push(item);
-    arr.push(obj[item]);
-    areaPieChartArr.push(arr);
-  })
-  areaPieChartArr = [areaPieChartArr[1], areaPieChartArr[2], areaPieChartArr[0]];
-  const chart = c3.generate({
-    bindto: ".pie-chart",
-    data: {
-      columns: areaPieChartArr,  // 格式 [["高雄", 2], ["台北",1], ["台中", 1]]
-      type : 'donut',
-    },
-    donut: {
-      title: "地區"
-    }
-  });
-}
+// Javascript執行位置
+const addTicketForm = document.querySelector('#addTicketForm');
+const ticketName = document.querySelector('#ticketName');
+const ticketImgUrl = document.querySelector('#ticketImgUrl');
+const ticketArea = document.querySelector('#ticketArea');
+const ticketPrice = document.querySelector('#ticketPrice');
+const ticketNum = document.querySelector('#ticketNum');
+const ticketRate = document.querySelector('#ticketRate');
+const ticketDescription = document.querySelector('#ticketDescription');
+const inputDoms = document.querySelectorAll('[data-use="validate"]');
+const addTicketBtn = document.querySelector('#addTicketBtn');
+const areaSelect = document.querySelector('#areaSelect');
+const cardList = document.querySelector('#cardList');
+const filterText = document.querySelector('.filter-text');
 
-// JS執行位置
-const addTicketBtn = document.querySelector('.addTicket-btn');
-const inputDom = document.querySelectorAll('[data-use="validate"]');
-const regionSearch = document.querySelector('.regionSearch');
-
-let data = {};
+let data = [];
 const api = 'https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json';
 axios.get(api).then(res => {
   data = res.data.data;
-  renderTourCard(data);
-  renderChart()
+  renderTicket(data);
+  renderPieChart();
 })
 
-regionSearch.addEventListener('change', filterArea);
-addTicketBtn.addEventListener('click', addTourTicket);
-inputDom.forEach(item => {
-  item.addEventListener('blur', validate);
-});
+addTicketBtn.addEventListener('click', addTicket);
+inputDoms.forEach(inputDom => inputDom.addEventListener('blur', validate));
+areaSelect.addEventListener('change', filterArea);
